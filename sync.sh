eeef72a1dea1bf18291d2268716e950116c17b20
Evan Teitelman <teitelmanevan@gmail.com>
2015-01-25 17:39:57 -0500

    sync.sh: Add --chmod

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avzl --exclude={common,data,generate-site.sh,update-tools.sh,generators*,sync.sh,.git,.gitignore} . blackarch.org:/nginx/var/www/.new-site/.
+rsync --chmod "a+rX,ug+w" -avzl --exclude={common,data,generate-site.sh,update-tools.sh,generators*,sync.sh,.git,.gitignore} ./ blackarch.org:/nginx/var/www
