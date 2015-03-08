a89fb38402c30a14325ce2aee38e06fb11ca6ee6
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-03-08 19:35:31 +0100

    update mirrors

--- generators/downloads.html.sh
+++ generators/downloads.html.sh
@@ -375,9 +375,9 @@ cat <<\EOF
                             <a href="rsync://mirror.tillo.ch/blackarch/" target="_blank">rsync://mirror.tillo.ch/blackarch/</a>
                             <br /><br />
                             <img src="/images/flags/tr.png" /> Turkey<br />
-                            <a href="http://ftp.linux.org.tr/blackarch/" target="_blank">http://ftp.linux.org.tr/blackarch/</a<br />
-                            <a href="ftp://ftp.linux.org.tr/blackarch/" target="_blank">ftp://ftp.linux.org.tr/blackarch/</a<br />
-                            <a href="rsync://rsync.linux.org.tr/blackarch/" target="_blank">rsync://rsync.linux.org.tr/blackarch/</a
+                            <a href="http://ftp.linux.org.tr/blackarch/" target="_blank">http://ftp.linux.org.tr/blackarch/</a><br />
+                            <a href="ftp://ftp.linux.org.tr/blackarch/" target="_blank">ftp://ftp.linux.org.tr/blackarch/</a><br />
+                            <a href="rsync://rsync.linux.org.tr/blackarch/" target="_blank">rsync://rsync.linux.org.tr/blackarch/</a>
                             <br /><br />
                             <img src="/images/flags/tw.png" /> Taiwan<br />
                             <a href="http://blackarch.cs.nctu.edu.tw/" target="_blank">http://blackarch.cs.nctu.edu.tw/</a><br />
