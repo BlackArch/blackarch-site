f5bdeaf03ccddace45f68c87d385861ab9ae5f0a
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-04-20 00:13:51 +0200

    fix path. move main mirror to france

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync --chmod "a+rX,ug+w" -avzl --exclude={common,data,generate-site.sh,update-tools.sh,generators*,sync.sh,.git,.gitignore} ./ blackarch.org:/nginx/var/www
+rsync --chmod "a+rX,ug+w" -avzl --exclude={common,data,generate-site.sh,update-tools.sh,generators*,sync.sh,.git,.gitignore} ./ blackarch.org:/home/nginx/var/www
