10031e68a8a47ce755a2758edbc0fe56c42fcf5d
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-11-21 13:05:44 +0100

    - adjust links for blackarch linux guide - rename files

--- generators/guide.html.sh
+++ generators/guide.html.sh
@@ -8,12 +8,12 @@ cat <<\EOF
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    <a>Wiki <small>BlackArch Linux Documentation</small></a>
+                    <a>Guide <small>BlackArch Linux Documentation</small></a>
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="index.html">Home</a>
                     </li>
-                    <li class="active">Wiki</li>
+                    <li class="active">Guide</li>
                 </ol>
             </div>
         </div>
@@ -21,12 +21,6 @@ cat <<\EOF
 
         <!-- Content Row -->
         <div class="row">
-            <!-- Sidebar Column -->
-            <div class="col-md-3">
-                <div class="list-group">
-                    <a href="#mirror-list" class="list-group-item">Test</a>
-                </div>
-            </div>
             <!-- Content Column -->
             <div class="col-md-9">
                 <p>
@@ -34,7 +28,7 @@ cat <<\EOF
 					<br />
 					<br />
 
-					We're looking for translators. Please contact us if you would like to translate the wiki from English to any other language.
+					We're looking for translators. Please contact us if you would like to translate the guide from English to any other language.
                 </p>
             </div>
         </div>
