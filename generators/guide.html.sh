1c4b80537b278196ed9d72429d18d7f0014a94e0
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2015-11-30 13:15:24 +0100

    add guide.html

---
+++ generators/guide.html.sh
@@ -0,0 +1,39 @@
+cat common/start
+
+cat <<\EOF
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    <a>Guide <small>BlackArch Linux Documentation</small></a>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Home</a>
+                    </li>
+                    <li class="active">Guide</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <!-- Content Column -->
+            <div class="col-md-12">
+                <p>
+                    This is the official guide to BlackArch Linux. It is quite new. Kindly excuse all typos and errors. Soon, this guide will contain all of the information
+                    about BlackArch Linux you could possible imagine. We offer the guide in PDF.
+                </p>
+                <p>
+                    To download the guide, click here: <a href="guide.pdf" target="_blank">guide.pdf</a>.
+                </p>
+                <p>
+                    <i>We're looking for translators. Please contact us if you would like to translate the guide from English to any other language.</i>
+                </p>
+
+EOF
+
+cat common/end
