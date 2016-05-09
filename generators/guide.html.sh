c29dfe05854cc12c930ab6e926862de360a5569f
Anyon3 <dimitri@linux.com>
2016-05-10 03:30:36 +0800

    Generate a validate html5

--- generators/guide.html.sh
+++ generators/guide.html.sh
@@ -3,9 +3,8 @@ cat common/start
 cat <<\EOF
     <!-- Page Content -->
     <div class="container">
-
         <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
+         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
                     <a>Guide <small>BlackArch Linux Documentation</small></a>
@@ -18,7 +17,6 @@ cat <<\EOF
             </div>
         </div>
         <!-- /.row -->
-
         <!-- Content Row -->
         <div class="row">
             <!-- Content Column -->
@@ -30,28 +28,26 @@ cat <<\EOF
                 <p>
                     To download the guide, please use the links below.
                 </p>
-                <p>
-                    <table width="100px">
+
+                <table id=guide>
                     <tr>
-                    <td><img src="/images/flags/us.png" alt="United States" /></td>
-                    <td><a href="blackarch-guide-en.pdf" target="_blank">English</a></td>
-                    <tr />
+		                <td><img src="/images/flags/us.png" alt="United States" /></td>
+		                <td><a href="blackarch-guide-en.pdf" target="_blank">English</a></td>
+                    </tr>
                     <tr>
-                    <td><img src="/images/flags/fr.png" alt="France" /></td>
-                    <td><a href="blackarch-guide-fr.pdf" target="_blank">French</a></td>
-                    <tr />
+		                <td><img src="/images/flags/fr.png" alt="France" /></td>
+		                <td><a href="blackarch-guide-fr.pdf" target="_blank">French</a></td>
+                    </tr>
                     <tr>
-                    <td><img src="/images/flags/tr.png" alt="Turkey" /></td>
-                    <td><a href="blackarch-guide-tr.pdf" target="_blank">Turkish</a></td>
-                    <tr />
-                    </table>
-                </p>
+		                <td><img src="/images/flags/tr.png" alt="Turkey" /></td>
+		                <td><a href="blackarch-guide-tr.pdf" target="_blank">Turkish</a></td>
+                    </tr>
+                </table>
                 <p>
                     <i>We're looking for translators. Please use the latex file from
                     <a href="https://github.com/BlackArch/blackarch-guide/tree/master/latex" target="_blank">blackarch-guide</a> at <a href="https://github.com/" target="_blank">github</a>
                     and send us your changes or open a pull request if you would like to translate the guide from English to any other language.</i>
                 </p>
-
 EOF
 
 cat common/end
