34d0df9d173fd5f451ed962cfec9fe6bf6dbcba7
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2018-06-29 19:47:54 +0200

    fix identing

--- update-tools.sh
+++ update-tools.sh
@@ -14,78 +14,77 @@ OUT="data/tools"
 find data ! -name 'mirrors' -type f -exec rm -f {} +
 
 make_tmp() {
-    tmp=$(mktemp -d /tmp/blackarch.XXXXXXXXXXX)
+  tmp=$(mktemp -d /tmp/blackarch.XXXXXXXXXXX)
 }
 
 get_db() {
-    curl "https://$SITE/blackarch/$REPO/os/$ARCH/$REPO.db.tar.gz" |
-	tar xz -C "$tmp"
+  curl "https://$SITE/blackarch/$REPO/os/$ARCH/$REPO.db.tar.gz" |
+  tar xz -C "$tmp"
 }
 
 parse_db() {
-    mkdir -p "$(dirname file)"
+  mkdir -p "$(dirname file)"
 
-    for d in "$tmp"/*
-    do
-	# Name
-        name="$(grep --no-group-separator -A2 '^%NAME%$' "${d}"/desc |
-        sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%NAME%$')"
+  for d in "$tmp"/*
+  do
+	  # Name
+    name="$(grep --no-group-separator -A2 '^%NAME%$' "${d}"/desc |
+    sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%NAME%$')"
 
-	# Version
-        vers="$(grep --no-group-separator -A2 '^%VERSION%$' "${d}"/desc |
-        sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%VERSION%$')"
+	  # Version
+    vers="$(grep --no-group-separator -A2 '^%VERSION%$' "${d}"/desc |
+    sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%VERSION%$')"
 
-	# Description
-        desc="$(grep --no-group-separator -A2 '^%DESC%$' "${d}"/desc |
-        sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%DESC%$')"
+	  # Description
+    desc="$(grep --no-group-separator -A2 '^%DESC%$' "${d}"/desc |
+    sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%DESC%$')"
 
-	# Category
-	# Add exception for the following packages
-	case "${name}" in
+	  # Category
+	  # Add exception for the following packages
+	  case "${name}" in
       "blackarch-config-awesome"|"blackarch-config-fluxbox"|"blackarch-config-openbox"|"blackarch-config-i3"|"blackarch-config-spectrwm"|"blackarch-config-wmii"|"blackarch-config-lxdm"|"blackarch-config-vim"|"blackarch-config-bash"|"blackarch-config-x11"|"blackarch-config-gtk"|"blackarch-mirrorlist"|"blackarch-menus")
-	  group="blackarch-config"
-    ;;
+	      group="blackarch-config"
+        ;;
 	    "truecrack"|"cudahashcat"|"cryptohazemultiforcer")
-		group="blackarch-cracker"
-		;;
+		    group="blackarch-cracker"
+		    ;;
 	    "vmcloak"|"malboxes"|"thezoo")
-		group="blackarch-malware"
-		;;
+		    group="blackarch-malware"
+		    ;;
       *)
-	    	# All the other packages (add '0,/blackarch/s///' for remove first occurrence only
-		group="$(grep --no-group-separator -A2 '^%GROUPS%$' "${d}"/desc |
-		sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' -e '0,/blackarch/s///' | grep -v '^%GROUPS%$' |
-		tr -s '\n' ' ')"
+      # All the other packages (add '0,/blackarch/s///' for remove first occurrence only
+		  group="$(grep --no-group-separator -A2 '^%GROUPS%$' "${d}"/desc |
+      sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' -e '0,/blackarch/s///' | grep -v '^%GROUPS%$' |
+      tr -s '\n' ' ')"
 	esac
 
-	# Website url
-        url="$(grep --no-group-separator -A2 '^%URL%$' "${d}"/desc |
-        sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%URL%$')"
+    # Website url
+    url="$(grep --no-group-separator -A2 '^%URL%$' "${d}"/desc |
+    sed -e 's/[0-9]\+://' -e 's/-[0-9]\+//' | grep -v '^%URL%$')"
 
-	fgroup=$(echo "$group" | sed -e 's/blackarch-//g' -e 's/ //g' -e "s/'//g")
+	  fgroup=$(echo "$group" | sed -e 's/blackarch-//g' -e 's/ //g' -e "s/'//g")
 
-	# Do not insert the current package if the $group variable is empty
-	if [ "$group" ]; then
-	    echo "$name|$vers|$desc|$group|$url" >> "$OUT"
+	  # Do not insert the current package if the $group variable is empty
+	  if [ "$group" ]; then
+      echo "$name|$vers|$desc|$group|$url" >> "$OUT"
 	    if [ "$fgroup" ];  then
-		# [review] This was happening at least three times.
-		echo "$name|$vers|$desc|$url" >> data/"$fgroup"
+		    # [review] This was happening at least three times.
+		    echo "$name|$vers|$desc|$url" >> data/"$fgroup"
 	    fi
-        fi
-    done
+    fi
+  done
 }
 
 split() {
-    sed -i 's/\t/\|/g' "$OUT"
+  sed -i 's/\t/\|/g' "$OUT"
 }
 
 main() {
-
-    rm -f "$OUT"
-    make_tmp
-    get_db
-    parse_db
-    split
+  rm -f "$OUT"
+  make_tmp
+  get_db
+  parse_db
+  split
 }
 
 main "$@"
