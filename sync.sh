2b6505643592726f8c1f49f99eb3269dfef09d4e
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2019-05-23 13:45:39 +0200

    net64iso: update checksum. sync.sh: remove not needed exclusion.

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync --chmod "a+rX,ug+w" -avzl --exclude={README.md,common,data,*.sh,generators*,sync.sh,.git,.gitignore} ./ blackarch.org:/var/www
+rsync --chmod "a+rX,ug+w" -avzl --exclude={README.md,common,data,*.sh,generators*,.git,.gitignore} ./ blackarch.org:/var/www
