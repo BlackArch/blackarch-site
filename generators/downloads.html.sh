2e8de2d765eaab74f152fd05e3efc0bdf56091d6
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2020-04-05 14:28:12 +0200

    change default mirror.

--- generators/downloads.html.sh
+++ generators/downloads.html.sh
@@ -59,14 +59,14 @@ cat << EOF
                   <td><b>SHA1sum</b></td>
                 </tr>
                 <tr>
-                  <td><a href="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-live-2020.01.01-x86_64.iso" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit Live ISO</a></td>
+                  <td><a href="https://blackarch.unixpeople.org/iso/blackarch-linux-live-2020.01.01-x86_64.iso" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit Live ISO</a></td>
                   <td itemprop="datePublished">2020.01.01</td>
                   <td><a href="/blackarch/torrent/blackarch-linux-live-2020.01.01-x86_64.iso.torrent" itemprop="fileFormat" type="application/octet-stream">Torrent</a></td>
                   <td>14 GB</td>
                   <td>${LIVE_CHECKSUM}</td>
                 </tr>
                 <tr>
-                  <td><a href="https://ftp.halifax.rwth-aachen.de/blackarch/iso/blackarch-linux-netinst-2020.01.01-x86_64.iso" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit Netinstall ISO</a></td>
+                  <td><a href="https://blackarch.unixpeople.org/iso/blackarch-linux-netinst-2020.01.01-x86_64.iso" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit Netinstall ISO</a></td>
                   <td itemprop="datePublished">2020.01.01</td>
                   <td><a href="/blackarch/torrent/blackarch-linux-netinst-2020.01.01-x86_64.iso.torrent" itemprop="fileFormat" type="application/octet-stream">Torrent</a></td>
                   <td>491 MB</td>
@@ -113,7 +113,7 @@ cat << EOF
                   <td><b>SHA1sum</b></td>
                 </tr>
                 <tr>
-                  <td><a href="https://ftp.halifax.rwth-aachen.de/blackarch/ova/blackarch-linux-2020.01.01.ova" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit OVA Image</a></td>
+                  <td><a href="https://blackarch.unixpeople.org/ova/blackarch-linux-2020.01.01.ova" itemprop="fileFormat" type="application/x-iso9660-image">BlackArch Linux 64 bit OVA Image</a></td>
                   <td itemprop="datePublished">2020.01.01</td>
                   <td><a href="/blackarch/torrent/blackarch-linux-2020.01.01.ova.torrent" itemprop="fileFormat" type="application/octet-stream">Torrent</a></td>
                   <td>35 GB</td>
