55855249a56fc4ef4dd7660edc90b0ca4035baf8
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2025-12-05 00:08:27 +0100

    data: update tools list.

--- generators/downloads.html.sh
+++ generators/downloads.html.sh
@@ -291,7 +291,9 @@ cat << EOF
               <a href="rsync://ftp.halifax.rwth-aachen.de/blackarch/" target="_blank" itemprop="url">rsync://ftp.halifax.rwth-aachen.de/blackarch/</a><br>
               <a href="http://blackarch.unixpeople.org/" target="_blank" itemprop="url">http://blackarch.unixpeople.org/</a><br>
               <a href="https://blackarch.unixpeople.org/" target="_blank" itemprop="url">https://blackarch.unixpeople.org/</a><br>
-              <a href="rsync://blackarch.unixpeople.org/blackarch/" target="_blank" itemprop="url">rsync://blackarch.unixpeople.org/blackarch/</a>
+              <a href="rsync://blackarch.unixpeople.org/blackarch/" target="_blank" itemprop="url">rsync://blackarch.unixpeople.org/blackarch/</a><br>
+              <a href="http://mirror.diyarciftci.xyz/blackarch/" target="_blank" itemprop="url">http://mirror.diyarciftci.xyz/blackarch/</a><br>
+              <a href="https://mirror.diyarciftci.xyz/blackarch/" target="_blank" itemprop="url">https://mirror.diyarciftci.xyz/blackarch/</a>
               <br><br>
               <img src="/images/flags/gr.png" alt="Greece" /> Greece<br>
               <a href="http://ftp.cc.uoc.gr/mirrors/linux/blackarch/" target="_blank" itemprop="url">http://ftp.cc.uoc.gr/mirrors/linux/blackarch/</a><br>
